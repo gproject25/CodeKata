65d58e9ac3a902250b2c0ae97954383f32596765
gproject <juhan913@gmail.com>
2026-09-11 11:04:59 +0900

    2. 두 수의 곱 구하기

---
+++ 2.두수의곱/Multiply.java
@@ -0,0 +1,10 @@
+public class Multiply {
+    public int solution(int num1, int num2){
+        return num1*num2;
+    }
+    
+    public static void main(String[] args) {
+        Multiply multiply = new Multiply();
+        System.out.println(multiply.solution(27,19));
+    }
+}
